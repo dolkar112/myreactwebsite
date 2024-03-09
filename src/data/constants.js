149ae23218fa6d4d09a30f11b50dd4974eeef7fa
dolkar112 <155144476+dolkar112@users.noreply.github.com>
2024-03-09 16:55:08 +0600

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/dolkar112",
   resume:
-   https://drive.google.com/file/d/1Esd0hjsN0KbF5Jq98wsw9zSjEmVAwnM8/view?usp=sharing
+   https://drive.google.com/file/d/1Esd0hjsN0KbF5Jq98wsw9zSjEmVAwnM8/view
   linkedin: "https://www.linkedin.com",
   twitter: "https://twitter.com",
   insta: "https://www.instagram.com/",
